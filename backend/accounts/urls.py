ee7ed9cae75e54ba38d952eede3d503721ee02a2
Olha <olusiabn@gmail.com>
2023-06-12 19:30:17 +0200

    Add Token authentication

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,17 +2,28 @@
 URL configuration for accounts.
 """
 
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework import routers
 
 from .views import GroupViewSet, UserViewSet
 
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register(r"users", UserViewSet)
 router.register(r"groups", GroupViewSet)
+print(router.urls)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    # path("api/register/", RegisterAPI.as_view(), name="register"),
+
+    # session authentication
+    path("api-session-auth/", include("rest_framework.urls", namespace="rest_framework")),
+
+    # Token authentication
+
+    # http://127.0.0.1:8000/api-auth/users/
+    path(r'api-auth/', include('djoser.urls')),
+
+    # http://127.0.0.1:8000/auth/token/login/
+    # http://127.0.0.1:8000/auth/token/logout/
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
