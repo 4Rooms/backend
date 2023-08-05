3840d322ae0cc0ac94444952686f1310dc7c3bef
Olha <olusiabn@gmail.com>
2023-08-05 13:27:45 +0200

    Changed the chat model and added a URL field

--- backend/chat/serializer.py
+++ backend/chat/serializer.py
@@ -10,8 +10,15 @@ class ChatSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Chat
-        fields = ["id", "title", "room", "img", "creator", "description", "timestamp"]
+        fields = ["id", "title", "room", "img", "creator", "description", "url", "timestamp"]
         extra_kwargs = {"id": {"read_only": True}, "creator": {"read_only": True}}
+
+    def update_url(self, obj, *args, **kwargs):
+        """Update chat to save url with id"""
+
+        obj.url = f"/chat/{obj.room}/{obj.pk}/"
+        obj.save()
+        return obj
 
     def get_creator(self, obj):
         """Return creator username (instead id)"""
