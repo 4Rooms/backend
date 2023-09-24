a66d535ebad7011fdc1868211afd9bce75765614
Olha <olusiabn@gmail.com>
2023-09-24 12:22:02 +0200

    Add OnlineUser model MT-233

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -106,3 +106,13 @@ class Message(models.Model):
         self.is_deleted = True
         self.text = None
         self.save()
+
+
+class OnlineUser(models.Model):
+    """The user who is currently in a certain chat"""
+
+    user = models.ForeignKey(get_user_model(), related_name="group_user", on_delete=models.CASCADE, null=True)
+    chat = models.ForeignKey(Chat, related_name="group_participant", on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"user: {self.user}, chat: {self.chat}"
