987d84b54535389866f3368658de86d71c11b102
Olha <olusiabn@gmail.com>
2023-09-29 13:05:07 +0200

    Add SavedChat model MT-247

--- backend/chat/admin.py
+++ backend/chat/admin.py
@@ -1,6 +1,7 @@
-from chat.models import Chat, Message, OnlineUser
+from chat.models import Chat, Message, OnlineUser, SavedChat
 from django.contrib import admin
 
 admin.site.register(Chat)
 admin.site.register(Message)
 admin.site.register(OnlineUser)
+admin.site.register(SavedChat)
