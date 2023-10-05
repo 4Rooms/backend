9dd39797310dd42c4b160fd8d0a8aeac4ecea64e
Olha <olusiabn@gmail.com>
2023-10-05 10:47:15 +0200

    Add API fot getting My Chats. MT-246

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -4,6 +4,7 @@ from backend.chat.views import (
     ChatAPIView,
     DeleteSavedChatApiView,
     MessagesApiView,
+    MyChatsApiView,
     SavedChatApiView,
     UpdateDeleteChatApiView,
     UpdateDeleteMessageApiView,
@@ -11,6 +12,7 @@ from backend.chat.views import (
 
 urlpatterns = [
     path("chat/saved_chats/", SavedChatApiView.as_view(), name="get_saved_chats"),
+    path("chat/my_chats/", MyChatsApiView.as_view(), name="my_chats"),
     path("chat/<int:pk>/", UpdateDeleteChatApiView.as_view(), name="update_delete_chat"),
     path("chat/<str:room_name>/", ChatAPIView.as_view(), name="get_post_chats"),
     path("chat/<int:chat_id>/messages/", MessagesApiView.as_view(), name="get_messages"),
