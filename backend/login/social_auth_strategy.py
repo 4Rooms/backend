51c959c0b587da4201676a834b69aa7851b2b74a
Approximator <test@test>
2023-10-08 13:22:19 +0000

    Send frontend url in confirmation emails (MT-242)
    Send frontend URL even for requests made from backend

--- backend/login/social_auth_strategy.py
+++ backend/login/social_auth_strategy.py
@@ -8,11 +8,18 @@ It is necessary to set SOCIAL_AUTH_STRATEGY to point to this class in settings.p
 """
 
 
+import logging
+from urllib.parse import urljoin
+
+from config.utils import get_ui_host
+from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.shortcuts import resolve_url
 from login.cookie import set_auth_cookie
 from rest_framework_simplejwt.tokens import RefreshToken
 from social_django.strategy import DjangoStrategy
+
+logger = logging.getLogger(__name__)
 
 
 class AuthStrategy(DjangoStrategy):
@@ -27,10 +34,16 @@ class AuthStrategy(DjangoStrategy):
         """
         Redirect to a custom url after login and set a cookie with JWT token
         """
-        response = HttpResponseRedirect(resolve_url(url))
+
+        redirect_url = resolve_url(url)
+        response = HttpResponseRedirect(redirect_url)
+
+        if redirect_url == settings.LOGIN_REDIRECT_URL:
+            redirect_url = urljoin(get_ui_host(self.request), redirect_url)
 
         user = self.request.user
         token = RefreshToken.for_user(user).access_token
         set_auth_cookie(response, str(token))
 
+        logger.info(f"Redirecting to {redirect_url}")
         return response
