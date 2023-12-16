4619f64686dfcf59ee0502bdc639b1ef033131d2
Approximator <test@test>
2023-12-16 19:52:29 +0000

    Delete Online Users on startup (MT-298)

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -1,3 +1,7 @@
+import logging
+
+from channels.db import database_sync_to_async
+from chat.models.onlineUser import OnlineUser
 from chat.views.chat import (
     ChatGetAPIView,
     ChatPostAPIView,
@@ -11,7 +15,11 @@ from chat.views.chat import (
     UpdateDeleteChatApiView,
 )
 from chat.views.message import MessagesApiView, UpdateDeleteMessageApiView
+from django.core.exceptions import SynchronousOnlyOperation
+from django.db import connection
 from django.urls import path
+
+logger = logging.getLogger(__name__)
 
 urlpatterns = [
     path("chat/saved_chats/get/<str:room_name>/", GetSavedChatApiView.as_view(), name="get_saved_chats"),
@@ -37,3 +45,28 @@ urlpatterns = [
     # It is WS event
     path("chat/message/update_delete/<int:pk>/", UpdateDeleteMessageApiView.as_view(), name="update_delete_message"),
 ]
+
+
+def delete_online_users():
+    # checl if OnlineUsers table exists
+    all_tables = connection.introspection.table_names()
+    logger.info(f"Checking if OnlineUser table exists in DB: {all_tables}")
+    if "chat_onlineuser" not in all_tables:
+        logger.info("OnlineUser table does not exist - OK")
+        return
+
+    # delete online users if any
+    count = OnlineUser.objects.all().count()
+    if count > 0:
+        logger.info(f"Found {count} online users - deleting all...")
+        OnlineUser.objects.all().delete()
+        logger.info("Deleted all online users")
+    else:
+        logger.info("No online users found - OK")
+
+
+try:
+    delete_online_users()
+except SynchronousOnlyOperation:
+    logger.info("Could not delete online users. Use async mode")
+    database_sync_to_async(delete_online_users)()
