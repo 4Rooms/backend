24b434b308556157a73d89898913fc5755abc69e
Approximator <test@test>
2024-01-07 21:09:40 +0000

    Add `/` to password related API URLs (MT-550)

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -15,9 +15,9 @@ from backend.accounts.views import (
 urlpatterns = [
     # user
     path("user/", UserAPIView.as_view(), name="user"),
-    path("user/change-password", ChangePasswordAPIView.as_view(), name="change_password"),
-    path("user/password/request-reset", RequestPasswordResetAPIView.as_view(), name="request_password_reset"),
-    path("user/password/reset", PasswordResetAPIView.as_view(), name="password_reset"),
+    path("user/change-password/", ChangePasswordAPIView.as_view(), name="change_password"),
+    path("user/password/request-reset/", RequestPasswordResetAPIView.as_view(), name="request_password_reset"),
+    path("user/password/reset/", PasswordResetAPIView.as_view(), name="password_reset"),
     # profile
     path("profile/", ProfileAPIView.as_view(), name="profile"),
 ]
